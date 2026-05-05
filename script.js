745a4763a33a77d2e8e15cc6ac03e8fcd9ed1777
Nyxsve-C <nyx.sve.c@gmail.com>
2026-05-05 16:05:12 -0600

    Add input functionality

---
+++ script.js
@@ -0,0 +1,61 @@
+class Calculator {
+    input;
+    operator;
+    #display;
+
+    get display() {
+        return this.#display;
+    }
+
+    add() {
+        this.#display = (this.#display || 0) + this.input;
+    }
+
+    subtract() {
+        this.#display = (this.#display || 0) - this.input;
+    }
+
+    multiply() {
+        this.#display = (this.#display || 0) * this.input;
+    }
+
+    divide() {
+        this.#display = (this.#display || 0) / this.input;
+    }
+
+    operate() {
+        if (this.operator === 'divide' && this.input === 0) {
+            this.#display = 'error';
+            return
+        }
+        if (this.#display === 'error') {
+            this.#display === 0;
+        }
+        this[this.operator]();
+        this.input = null;
+    }
+
+    clear() {
+        this.#display = null;
+    }
+
+    back() {
+        this.#display = Math.floor(this.#display / 10);
+    }
+}
+
+const display = document.querySelector('.display');
+let value = display.value;
+const numbers = document.querySelectorAll('.numbers *');
+const dotButton = document.getElementById('dot');
+
+numbers.forEach(button => {
+    button.addEventListener('click', e => {
+        const input = e.target.innerText;
+        console.log(e.target.innerText);
+        value += input;
+        if (input === '.') {
+            dotButton.disabled = true;
+        }
+    });
+});
