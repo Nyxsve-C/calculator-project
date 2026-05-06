ce68b5cf81c937f8fec4aa070735c6d19898d985
Nyxsve-C <nyx.sve.c@gmail.com>
2026-05-05 18:34:33 -0600

    Improve input functionality

--- script.js
+++ script.js
@@ -1,60 +1,67 @@
-class Calculator {
-    input;
-    operator;
-    #display;
+// class Calculator {
+//     input;
+//     operator;
+//     #display;
 
-    get display() {
-        return this.#display;
-    }
+//     get display() {
+//         return this.#display;
+//     }
 
-    add() {
-        this.#display = (this.#display || 0) + this.input;
-    }
+//     add() {
+//         this.#display = (this.#display || 0) + this.input;
+//     }
 
-    subtract() {
-        this.#display = (this.#display || 0) - this.input;
-    }
+//     subtract() {
+//         this.#display = (this.#display || 0) - this.input;
+//     }
 
-    multiply() {
-        this.#display = (this.#display || 0) * this.input;
-    }
+//     multiply() {
+//         this.#display = (this.#display || 0) * this.input;
+//     }
 
-    divide() {
-        this.#display = (this.#display || 0) / this.input;
-    }
+//     divide() {
+//         this.#display = (this.#display || 0) / this.input;
+//     }
 
-    operate() {
-        if (this.operator === 'divide' && this.input === 0) {
-            this.#display = 'error';
-            return
-        }
-        if (this.#display === 'error') {
-            this.#display === 0;
-        }
-        this[this.operator]();
-        this.input = null;
-    }
+//     operate() {
+//         if (this.operator === 'divide' && this.input === 0) {
+//             this.#display = 'error';
+//             return
+//         }
+//         if (this.#display === 'error') {
+//             this.#display === 0;
+//         }
+//         this[this.operator]();
+//         this.input = null;
+//     }
 
-    clear() {
-        this.#display = null;
-    }
+//     clear() {
+//         this.#display = null;
+//     }
 
-    back() {
-        this.#display = Math.floor(this.#display / 10);
-    }
-}
+//     back() {
+//         this.#display = Math.floor(this.#display / 10);
+//     }
+// }
 
 const display = document.querySelector('.display');
 const numbers = document.querySelectorAll('.numbers *');
-const dotButton = document.getElementById('dot');
+const dot = document.getElementById('dot');
 
 numbers.forEach(button => {
     button.addEventListener('click', e => {
         const input = e.target.innerText;
         console.log(e.target.innerText);
-        display.value += input;
-        if (input === '.') {
-            dotButton.disabled = true;
-        }
+        if (display.value === '0') {
+            display.value = input;
+        } else {
+            display.value += input;
+        };
+        if (display.value.includes('.')) {
+            dot.disabled = true;
+            if (display.value === '.') {
+                display.value = '0.';
+            }
+        };
     });
 });
