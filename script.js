b282c031766711434692002ba8df752111679536
Nyxsve-C <nyx.sve.c@gmail.com>
2026-05-12 18:14:47 -0600

    Add keyboard support

--- script.js
+++ script.js
@@ -45,11 +45,18 @@
 // }
 
 const keys = {
-    numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '.'],
-    dot: '.',
-    erase: ['c', 'Backspace'],
-    operators: ['/', '*', '-', '+', 'Enter'],
-    equals: '=',
+    numbers: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'],
+    operators: {
+        '/': '÷',
+        '*': '×',
+        '-': '-',
+        '+': '+',
+    },
+    'Enter': '=',
+    erase: {
+        'c': 'C', 
+        'Backspace': 'Back',
+    },
 }
 
 const operations = {
@@ -172,4 +179,102 @@ erase.forEach(button => {
         }
         updateState();
     });
+});
+
+document.addEventListener('keydown', e => {
+    const key = e.key;
+    if (keys.numbers.includes(key)) {
+        const input = key;
+        console.log(input);
+        erase[1].disabled = false;
+        operators.forEach(operator => operator.disabled = false);
+        if (clearDisplayScheduled) {
+            display.value = '';
+            clearDisplayScheduled = false;
+        }
+        if (display.value === '0') {
+            display.value = input;
+        } else {
+            display.value += input;
+        }
+        if (display.value.includes('.')) {
+            dot.disabled = true;
+            if (display.value === '.') {
+                display.value = '0.';
+            }
+        }
+        if (state === 'waitingA') a = display.value, equals.disabled = true;        
+        if (state === 'waitingB') b = display.value;
+        console.log('display:', display.value, '\na:', a, '\nb:', b, '\noperator:', operator);
+        updateState();
+    }
+    if (!operators[0].disabled) {
+        if (key in keys.operators) {
+            if (!operator) {
+                operator = keys.operators[key];
+                console.log(operator);
+            } else if (!b) {
+                operator = keys.operators[key];
+                console.log(operator);
+            } else {
+                const result = Math.floor(operate(operator, +a, +b) * 100) / 100;
+                if (result === Infinity) {
+                    operator = a = b = '';
+                    operators.forEach(operator => operator.disabled = true);
+                    display.value = 'error';
+                } else {
+                    display.value = result;
+                    console.log(operator, result);
+                    operator = keys.operators[key];
+                    a = result;
+                    b = '';
+                    equals.disabled = true;
+                }
+            }
+            erase[1].disabled = true;
+            dot.disabled = false;
+            clearDisplayScheduled = true;
+            updateState();
+        }
+    }
+    if(key === 'Enter' && !equals.disabled) {
+        console.log(key);
+        const result = Math.floor(operate(operator, +a, +b) * 100) / 100;
+        if (result === Infinity) {
+            operator = a = b = '';
+            operators.forEach(operator => operator.disabled = true);
+            display.value = 'error';
+        } else {
+            display.value = result;
+            console.log(operator, result);
+            operator = keys.operators[key];
+            a = result;
+            b = '';
+            equals.disabled = true;
+        }
+    }
+    if (key in keys.erase) {
+        const action = keys.erase[key];
+        console.log(action);
+        if (action === 'C') {
+            display.value = '';
+            dot.disabled = false;
+            operator = a = b = '';
+            operators.forEach(operator => operator.disabled = true);
+        } else if (!erase[1].disabled) {
+            const last = display.value.slice(-1);
+            display.value = display.value.slice(0, -1);
+            if (last === '.') {
+                dot.disabled = false;
+            }
+            if (display.value === '') {
+                dot.disabled = false;
+                operators.forEach(operator => operator.disabled = true);
+            }
+            if (state === 'waitingA') a = display.value, equals.disabled = true;        
+            if (state === 'waitingB') b = display.value;
+            console.log('display:', display.value, 'a:', a, 'b:', b);
+        }
+        updateState();
+    }
 });
